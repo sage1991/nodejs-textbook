c1a826efd48407ff71f4c26d6dd709d6abc049c2
harry <shsage91@gmail.com>
2023-06-04 01:28:08 +0900

    Add create http server example

---
+++ packages/http-modules/src/create-server/server.ts
@@ -0,0 +1,31 @@
+import { createServer } from "http"
+import { resolve } from "path"
+import { readFile } from "fs/promises"
+
+const index = resolve(__dirname, "index.html")
+
+const server = createServer(async (req, res) => {
+  try {
+    const data = await readFile(index)
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    })
+    res.end(data)
+  } catch (e) {
+    console.error(e)
+    res.writeHead(500, {
+      "Content-Type": "text/plain; charset=utf-8"
+    })
+    res.end(e instanceof Error ? e.message : "Error")
+  }
+})
+
+server.listen(3000)
+
+server.on("listening", () => {
+  console.log("Server start at 3000 port")
+})
+
+server.on("error", (e) => {
+  console.error(e)
+})
